f2013ec8622c1f0d31ed093481f56cf9a5a889cc
Ed Hickey <erhickey@gmail.com>
2022-10-28 20:33:15 -0400

    Utilize Diagnostic signs
    Instead of allowing the user to specify the icons used in the
    statusline, re-use the same text that the Diagnostic Signs use.

--- lua/diagline-nvim.lua
+++ lua/diagline-nvim.lua
@@ -1,93 +1,80 @@
-local levels = {
+local severities = {
   {
-    level = vim.diagnostic.severity.ERROR,
-    color = '%#SLDiagnosticError#'
+    name = 'Error',
+    severity = vim.diagnostic.severity.ERROR
   },
   {
-    level = vim.diagnostic.severity.WARN,
-    color = '%#SLDiagnosticWarn#'
+    name = 'Warn',
+    severity = vim.diagnostic.severity.WARN
   },
   {
-    level = vim.diagnostic.severity.INFO,
-    color = '%#SLDiagnosticInfo#'
+    name = 'Info',
+    severity = vim.diagnostic.severity.INFO
   },
   {
-    level = vim.diagnostic.severity.HINT,
-    color = '%#SLDiagnosticHint#'
+    name = 'Hint',
+    severity = vim.diagnostic.severity.HINT
   },
 }
 
-local function create_diag_highlights()
+local function create_diag_statusline_highlights()
   local slbg = string.format('#%06x', vim.api.nvim_get_hl_by_name('StatusLine', true)['foreground'])
   local defg = string.format('#%06x', vim.api.nvim_get_hl_by_name('DiagnosticError', true)['foreground'])
   local dwfg = string.format('#%06x', vim.api.nvim_get_hl_by_name('DiagnosticWarn', true)['foreground'])
   local difg = string.format('#%06x', vim.api.nvim_get_hl_by_name('DiagnosticInfo', true)['foreground'])
   local dhfg = string.format('#%06x', vim.api.nvim_get_hl_by_name('DiagnosticHint', true)['foreground'])
-  vim.highlight.create('SLDiagnosticError', {guibg=slbg, guifg=defg})
-  vim.highlight.create('SLDiagnosticWarn', {guibg=slbg, guifg=dwfg})
-  vim.highlight.create('SLDiagnosticInfo', {guibg=slbg, guifg=difg})
-  vim.highlight.create('SLDiagnosticHint', {guibg=slbg, guifg=dhfg})
+  vim.highlight.create('SLDiagnosticError', { guibg=slbg, guifg=defg })
+  vim.highlight.create('SLDiagnosticWarn', { guibg=slbg, guifg=dwfg })
+  vim.highlight.create('SLDiagnosticInfo', { guibg=slbg, guifg=difg })
+  vim.highlight.create('SLDiagnosticHint', { guibg=slbg, guifg=dhfg })
 end
 
 local separator = '%='
 local file_name = ' %-f'
 local modified = ' %-m %-q'
-local diag_counts = '%{%luaeval("diagline_nvim_counts()")%}'
+local diag_counts = '%{%luaeval("Diagline_nvim_counts()")%}'
 local line_info = '%l,%c   %P '
 
-local presets = {
-  indicators = {
-    [vim.diagnostic.severity.ERROR] = '',
-    [vim.diagnostic.severity.WARN] = '',
-    [vim.diagnostic.severity.INFO] = '',
-    [vim.diagnostic.severity.HINT] = ''
-  },
-  statusline = string.format(
-    '%s%s%s%s%s%s',
-    file_name,
-    modified,
-    separator,
-    diag_counts,
-    separator,
-    line_info
-  ),
-}
-
 local module = {
-  options = presets
+  config = {
+    statusline = string.format(
+      '%s%s%s%s%s%s',
+      file_name,
+      modified,
+      separator,
+      diag_counts,
+      separator,
+      line_info
+    ),
+  }
 }
 
-function module.setup(opts)
-  if opts ~= nil then
-    if opts.indicators ~= nil then
-      for k, v in pairs(opts.indicators) do
-        module.options.indicators[k] = v
-      end
-    end
-
-    if opts.statusline ~= nil then
-      module.options.statusline = opts.statusline
-    end
+function module.setup(config)
+  if config ~= nil then
+    for k, v in pairs(config) do module.config[k] = v end
   end
 
-  function diagline_nvim_counts()
+  create_diag_statusline_highlights()
+
+  function Diagline_nvim_counts()
     local diags = {}
 
-    for _, l in pairs(levels) do
-      local level = l['level']
-      local color = l['color']
-      local ind = module.options.indicators[level]
-      local count = vim.tbl_count(vim.diagnostic.get(0, { severity = level }))
+    for _, severity in ipairs(severities) do
+      local name = severity['name']
+      local color = '%#SLDiagnostic' .. name .. '#'
+      local icon = vim.fn.sign_getdefined('DiagnosticSign' .. name)[1]['text']
+      local count = vim.tbl_count(vim.diagnostic.get(0, { severity = severity['severity'] }))
       if count > 0 then
-        table.insert(diags, color .. count .. " " .. ind)
+        table.insert(diags, color .. count .. ' ' .. icon)
       end
     end
 
-    return table.concat(diags, '  ') ..  '%#StatusLine#'
+    return table.concat(diags, ' ') ..  '%#StatusLine#'
   end
 
-  create_diag_highlights()
-  vim.opt.statusline = module.options.statusline
+  if type(module.config.statusline) == 'string' then
+    vim.opt.statusline = module.config.statusline
+  end
 end
 
 return module
